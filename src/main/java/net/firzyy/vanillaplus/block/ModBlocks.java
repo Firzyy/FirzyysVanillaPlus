fa0d5636fc5206abd129e707feddf52d5b21ffa9
Rhys Purcell <purcellrhys@gmail.com>
2024-03-02 15:30:58 +0000

    Added - Normal and Deepslate Ruby Ores with Ruby drops

--- src/main/java/net/firzyy/vanillaplus/block/ModBlocks.java
+++ src/main/java/net/firzyy/vanillaplus/block/ModBlocks.java
@@ -3,10 +3,12 @@ package net.firzyy.vanillaplus.block;
 import net.firzyy.vanillaplus.VanillaPlus;
 import net.firzyy.vanillaplus.item.ModItems;
 import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,6 +23,13 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block", () -> new Block
             (BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+
+    public static final RegistryObject<Block> RUBY_ORE = registerBlock("ruby_ore", () -> new DropExperienceBlock
+            (BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<Block> DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore", () -> new DropExperienceBlock
+            (BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                    .strength(4.5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
